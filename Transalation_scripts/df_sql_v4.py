cd8962ef9c381ba384b9c221f460df0d5c660f1c
Clive Binu <clive.binu2@gmail.com>
2023-08-04 09:50:24 -0500

    updated df_sql

--- Transalation_scripts/df_sql_v4.py
+++ Transalation_scripts/df_sql_v4.py
@@ -1,34 +1,35 @@
+#%%
 from helper import *
 import os
 from astropy.time import Time
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VLA_RFI_GUI_NEW.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gbt_rfi.settings')
 from django.core.wsgi import get_wsgi_application
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 application = get_wsgi_application()
-from gui.models import *
+from rfi.models import *
 import glob
 # Frequency,Source,Polarization,File,Project,FrequencyType,Session
 
 print('Creating Dataframe')
-
-f_path = '/Users/clive/codes/NRAO/VLA_RFI_GUI_NEW/gui/RFI_VLA_Sample_Merged/'
+f_path = '/Users/clive/codes/NRAO/VLA_RFI_GUI_NEW/gui/RFI_VLA_Sample_Merged'
 files = glob.glob(f'{f_path}/*')
-
+#%%
+files = sorted(files,key=lambda x: f"{x.split('/')[-1].split('_')[-2]}_{x.split('/')[-1].split('_')[-1].split('.')[0]}")
 # f = File(path=f_path,name=f_path.split('/')[-1])
 # f.save()
-
+#%%
 pr = Project(name=f_path.split('/')[-2])
 pr.save()
 
 df_records = pd.read_csv(files[0])
 df_records = df_records.to_dict('records')
-datetime = f'{df_records[0]["OBS. DATE"]} {df_records[0]["Time of record"]}'
-t = Time(datetime,format='iso')
-mjd = t.mjd
-source = df_records[0]["Source"]
+# datetime = f'{df_records[0]["OBS. DATE"]} {df_records[0]["Time of record"]}'
+# t = Time(datetime,format='iso')
+# mjd = t.mjd
+# source = df_records[0]["Source"]
 
-s = Source(name=source)
-s.save()
+# s = Source(name=source)
+# s.save()
 
 polarization = ['LL','RR']
 model_instances1 = [Polarization(name=polar) for polar in polarization]
@@ -42,30 +43,55 @@ ft_name = [i.name for i in ft]
 model_instances3 = [Frontend(name=f'{band}_Band_Reciever') for band in bands]
 front = Frontend.objects.bulk_create(model_instances3)
 
-counter = 1
-for file in files:
+fname_cheker = False
 
+# counter = 1
+for file in files:
+    
     df_records = pd.read_csv(file)
     df_records = df_records.to_dict('records')
     polarization = df_records[0]["Polar"]
     ftype = f"{file.split('/')[-1].split('_')[0]}"
-    print('Inserting Values to model')
 
     f = File(path=file,name=file.split('/')[-1])
     f.save()
 
+    if file.split('/')[-1].split('_')[-1].split('.')[0] != fname_cheker:
+        print(file.split('/')[-1].split('_')[-1].split('.')[0])
+        print(fname_cheker)
+        datetime = f'{df_records[0]["OBS. DATE"]} {df_records[0]["Time of record"]}'
+        t = Time(datetime,format='iso')
+        mjd = t.mjd
+        source = df_records[0]["Source"]
+    
+        s = Source(name=source)
+        s.save()
+
+        # ses = Session(name=f"rfi_scan_{file.split('/')[-1].split('_')[-2]}_{file.split('/')[-1].split('_')[-1].split('.')[0]}",project=pr,file=f)
+        # ses.save()
 
 
-    ses = Session(name=counter,project=pr,file=f)
-    ses.save()
+    print('Inserting Values to model')
+
+
+
+
+    # ses = Session(name='rfi_scan1',project=pr,file=f)
+    # ses.save()
 
     if polarization == 'LL':
         if ftype in ft_name:
+
+            ses = Session(name=f"rfi_scan_{file.split('/')[-1].split('_')[-2]}_{file.split('/')[-1].split('_')[-1].split('.')[0]}_{ftype}_{polarization}",project=pr,file=f)
+            ses.save()
+
             scan = Scan(session = ses,source=s,frequency_type=ft[ft_name.index(ftype)],
                         polarization=p[0],mjd=mjd,datetime=datetime,frontend=front[ft_name.index(ftype)])
     
     else:
         if ftype in ft_name:
+            ses = Session(name=f"rfi_scan_{file.split('/')[-1].split('_')[-2]}_{file.split('/')[-1].split('_')[-1].split('.')[0]}_{ftype}_{polarization}",project=pr,file=f)
+            ses.save()
             scan = Scan(session = ses,source=s,frequency_type=ft[ft_name.index(ftype)],
                 polarization=p[1],mjd=mjd,datetime=datetime,frontend = front[ft_name.index(ftype)])
     
@@ -79,4 +105,8 @@ for file in files:
 
 
     Frequency.objects.bulk_create(model_instances)
-    counter+=1
+    fname_cheker = file.split('/')[-1].split('_')[-1].split('.')[0]
+
+
+    # counter+=1
+# %%
